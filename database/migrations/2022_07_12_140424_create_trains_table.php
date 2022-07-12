63f3d17f9e3e99f231338025aa70ba69a41723df
salamone-gius <salamone.gius@gmail.com>
2022-07-12 18:04:43 +0200

    model and seeder created

--- database/migrations/2022_07_12_140424_create_trains_table.php
+++ database/migrations/2022_07_12_140424_create_trains_table.php
@@ -16,6 +16,9 @@ class CreateTrainsTable extends Migration
         // definisco il nome delle colonne della nuova tabella e il tipo di dato che le andrà a popolare
         // avvio la migration che crea le colonne della mia tabella con comando terminale 'php artisan migrate'
         // con comando terminale 'php artisan make:migration add_nome_colonna_column_nome_tabella_table --table=nome_tabella' aggiungo una nuova colonna
+        // con comando terminale 'php artisan make:model NomeTabellaAlSingolare' creo un Model che interagirà con il db
+        // con comando terminale 'php artisan make:seeder NomeTabella(in Pascal Case)TableSeeder' creo un file seeder attraverso cui potrò popolare la mia tabella
+
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company', 30);
