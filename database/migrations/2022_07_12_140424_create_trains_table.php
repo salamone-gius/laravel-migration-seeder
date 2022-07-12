9c77d64519bb66b5c8a44f7579c46f93d0c81d61
salamone-gius <salamone.gius@gmail.com>
2022-07-12 17:15:39 +0200

    columns created

--- database/migrations/2022_07_12_140424_create_trains_table.php
+++ database/migrations/2022_07_12_140424_create_trains_table.php
@@ -13,11 +13,25 @@ class CreateTrainsTable extends Migration
      */
     public function up()
     {
+        // definisco il nome delle colonne della nuova tabella e il tipo di dato che le andrà a popolare
+        // avvio la migration che crea le colonne della mia tabella con comando terminale 'php artisan migrate'
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->date('departure_date');
+            $table->date('arrival_date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->unsignedTinyInteger('wagons_number');
+            $table->boolean('is_in_time')->default(true);
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
