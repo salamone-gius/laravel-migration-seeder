55dc1a986926c9e244781c13b7623a8a80c1774d
salamone-gius <salamone.gius@gmail.com>
2022-07-13 11:29:02 +0200

    departure_date filtered

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -12,8 +12,10 @@ class TrainController extends Controller
     // creo il metodo che mi restituirà la view e i dati db
     public function index() {
 
-        // stampo tutti gli elementi
-        $trains = Train::all();
+        // stampo i treni in partenza oggi:
+        // - con 'where' (query builder) creo la query: primo paramentro = per quale colonna devo filtrare; secondo paramentro = operatore relazionale (qui è sottintenso quello di uguaglianza); terzo parametro = metodo date(formattato così sennò aggiunge anche l'orario) che restituisce ora e data odierni;
+        // - metodo get() obbligatorio
+        $trains = Train::where('departure_date', date('Y-m-d'))->get();
 
         return view('home', compact('trains'));
     }
